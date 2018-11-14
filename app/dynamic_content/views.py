05581d97a9ae6ab602015d6e0ddb40d39ef7580c
Nathan <nembery@gmail.com>
2018-11-14 09:50:52 -0500

    update title in dynamic content

--- app/dynamic_content/views.py
+++ app/dynamic_content/views.py
@@ -7,8 +7,7 @@ from pan_cnc.views import CNCBaseAuth, CNCBaseFormView
 class DownloadDynamicContentView(CNCBaseAuth, CNCBaseFormView):
     snippet = 'download_dynamic_content'
     header = 'Download Dynamic Content'
-    title = 'Where is this title?'
-    action = 'what is this action?'
+    title = 'Choose Package Type to Download'
     app_dir = 'dynamic_content'
 
     def form_valid(self, form):
