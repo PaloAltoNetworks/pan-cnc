04fcabef5d03759704bcb6503e269fa52258a95e
Nathan <nembery@gmail.com>
2019-08-02 11:33:28 -0400

    remove bash requirement

--- tools/init_virtual_env.sh
+++ tools/init_virtual_env.sh
@@ -1,49 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 VIRTUAL_PATH=$1
 
-if [[ ! -d $VIRTUAL_PATH ]];
+if [ ! -d "$VIRTUAL_PATH" ];
 then
     echo "Directory does not exist"
     exit 1
 fi
 
-cd "${VIRTUAL_PATH}"
+cd "${VIRTUAL_PATH}" || (echo "Could not change directory" && exit 1)
 
-if [[ $? -ne 0 ]];
-then
-    echo "Could not change directory"
-    exit 1
-fi
-
-if [[ -f .python3_init_done && -d .venv ]];
+if [ -f .python3_init_done ] && [ -d .venv ];
 then
     echo "Environment already set up"
     exit 0
 fi
 
-python3 -m virtualenv ./.venv
+python3 -m virtualenv ./.venv || (echo "Could not create virtualenv!"; exit 1)
 
-if [[ $? -ne 0 ]];
-then
-    echo "Could not create virtualenv"
-    exit 1
-fi
-
-if [[ ! -f requirements.txt ]];
+if [ ! -f requirements.txt ];
 then
     touch .python3_init_done
     exit 0
 fi
 
 echo "Installing requirements"
-./.venv/bin/pip3 install -r requirements.txt
-
-if [[ $? -ne 0 ]];
-then
-    echo "Could not install requirements"
-    exit 1
-fi
+./.venv/bin/pip3 install -r requirements.txt || (echo "Could not install requirements!"; exit 1)
 
 touch .python3_init_done
 
