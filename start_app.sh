b8f50bbda8f41e4165587e8d6a3f0feeb448de64
Nathan <nembery@gmail.com>
2019-08-02 11:55:50 -0400

    pep8 80 char nag

--- start_app.sh
+++ start_app.sh
@@ -7,7 +7,9 @@ celery -A pan_cnc worker --loglevel=info  &
 if [ ! -f /app/cnc/db.sqlite3 ];
   then
     python /app/cnc/manage.py migrate && \
-    python /app/cnc/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('${CNC_USERNAME}', 'admin@example.com', '${CNC_PASSWORD}')" \
+    python /app/cnc/manage.py shell -c \
+    "from django.contrib.auth.models import User; User.objects.create_superuser('${CNC_USERNAME}'," \
+    "'admin@example.com', '${CNC_PASSWORD}')" \
     || exit 1
 fi
 echo "====="
