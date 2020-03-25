ea87cc45a60bccbb51ae0a3ae28cce1058d92283
Nathan <nembery@gmail.com>
2020-03-25 16:37:27 -0400

    add create_docker_group tool

--- tools/create_docker_group.sh
+++ tools/create_docker_group.sh
@@ -3,17 +3,17 @@
 DOCKER_SOCKET=/var/run/docker.sock
 DOCKER_GID=$(stat -c %g ${DOCKER_SOCKET})
 
-if [ "${DOCKER_GID}" -eq "0" ];
- then
+if [ "${DOCKER_GID}" -eq "0" ]; then
   exit 0
 fi
 
 DOCKER_GROUP=$(grep "${DOCKER_GID}" /etc/group | cut -f1 -d':')
 
-if [ "${DOCKER_GROUP}" != "" ];
- then
-  echo "Adding user to existing group"
-  groupadd cnc_user "${DOCKER_GROUP}"
+if [ "${DOCKER_GROUP}" != "" ]; then
+  if ! groups cnc_user | grep -q "${DOCKER_GROUP}"; then
+    echo "Adding user to existing group"
+    groupadd cnc_user "${DOCKER_GROUP}"
+  fi
 else
   echo "Creating new group and adding user"
   groupadd -g "${DOCKER_GID}" cnc_docker
